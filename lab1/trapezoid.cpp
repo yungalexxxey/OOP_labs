3d933f33031ff3741614a95daf6a27d6503873ad
yungalexxxey <76864298+yungalexxxey@users.noreply.github.com>
2021-09-21 19:04:37 +0300

    Update trapezoid.cpp

--- lab1/trapezoid.cpp
+++ lab1/trapezoid.cpp
@@ -1,7 +1,7 @@
 #include "trapezoid.h"
 #include <math.h>
 
-trapezoid::trapezoid(std::istream&is){
+trapezoid::Trapezoid(std::istream&is){
     std::cout <<"set x1 and y1:";
     is >> x1 >> y1;
     std::cout <<"set x2 and y2:";
@@ -11,19 +11,19 @@ trapezoid::trapezoid(std::istream&is){
     std::cout <<"set x4 and y4:";
     is >> x4 >> y4;
 }
-void trapezoid::Print(std::ostream&os){
+void Trapezoid::Print(std::ostream&os){
     os << "Trapezoid " << "(" <<x1<<" "<<y1<<")"<< "(" <<x2<<" "<<y2<<")"<< "(" <<x3<<" "<<y3<<")"<< "(" <<x4<<" " <<y4<<")" <<std::endl;
 }
-size_t trapezoid::VertexesNumber(){
+size_t Trapezoid::VertexesNumber(){
     return 4;
 }
-bool trapezoid::isit(){
+bool Trapezoid::isit(){
 double k=(y1-y4)/(x1-x4);
 double k1=(y2-y3)/(x2-x3);
 if(k==k1) return true;
 else return false;
 }
-double trapezoid::Area(){
+double Trapezoid::Area(){
   double h=sqrt((y2-y1)*(y2-y1));
   double os1=sqrt((x4-x1)*(x4-x1)+(y1-y4)*(y1-y4));
   double os2=sqrt((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2));
