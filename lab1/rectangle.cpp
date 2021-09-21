eede727a827634875bc12330fe15caaec87c9706
yungalexxxey <76864298+yungalexxxey@users.noreply.github.com>
2021-09-21 18:03:36 +0300

    Update rectangle.cpp

--- lab1/rectangle.cpp
+++ lab1/rectangle.cpp
@@ -1,7 +1,7 @@
 #include "rectangle.h"
 #include <math.h>
 
-rectangle::rectangle(std::istream&is){
+Rectangle::Rectangle(std::istream&is){
     std::cout <<"set x1 and y1:";
     is >> x1 >> y1;
     std::cout <<"set x2 and y2:";
@@ -11,13 +11,13 @@ rectangle::rectangle(std::istream&is){
     std::cout <<"set x4 and y4:";
     is >> x4 >> y4;
 }
-void rectangle::Print(std::ostream&os){
+void Rectangle::Print(std::ostream&os){
     os << "Rectangle " << "(" <<x1<<" "<<y1<<")"<< "(" <<x2<<" "<<y2<<")"<< "(" <<x3<<" "<<y3<<")"<< "(" <<x4<<" " <<y4<<")" <<std::endl;
 }
-size_t rectangle::VertexesNumber(){
+size_t Rectangle::VertexesNumber(){
     return 4;
 }
-bool rectangle::isit(){
+bool Rectangle::isit(){
 double perp;
 double perp2;
 perp=(x4-x1)*(x2-x1)+(y4-y1)*(y2-y1);
@@ -26,7 +26,7 @@ if((perp+perp2)==0) return true;
 else return false;
 
 }
-double rectangle::Area(){
+double Rectangle::Area(){
     double r1 = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     double r2 = sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
     double r3 = sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
