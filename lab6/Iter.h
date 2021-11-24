7e32f4a06fdf95f7d1a61314b215b46d44b35461
yungalexxxey <76864298+yungalexxxey@users.noreply.github.com>
2021-11-24 12:10:11 +0300

    fixes
    Удалил лишние файлы .gch

--- lab6/Iter.h
+++ lab6/Iter.h
@@ -4,8 +4,9 @@
 #include <memory>
 
 template <class node, class T>
-class Iter {
- public:
+class Iter
+{
+public:
   Iter(std::shared_ptr<node> n) { node_ptr = n; }
 
   std::shared_ptr<T> operator*() { return node_ptr->Get(); }
@@ -13,17 +14,18 @@ class Iter {
   std::shared_ptr<T> operator->() { return node_ptr->Get(); }
   void operator++() { node_ptr = node_ptr->GetNext(); }
 
-  Iter operator++(int) {
+  Iter operator++(int)
+  {
     Iter iter(*this);
     ++(*this);
     return iter;
   }
 
-  bool operator==(Iter const& i) { return node_ptr == i.node_ptr; }
+  bool operator==(Iter const &i) { return node_ptr == i.node_ptr; }
 
-  bool operator!=(Iter const& i) { return !(*this == i); }
+  bool operator!=(Iter const &i) { return !(*this == i); }
 
- private:
+private:
   std::shared_ptr<node> node_ptr;
 };
 
